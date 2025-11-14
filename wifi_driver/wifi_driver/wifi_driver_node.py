abd29a27bdaf9be025e96f5fec9ae4dd42daa21c
Jack Albertson <albertson.j@northeastern.edu>
2025-11-13 22:06:59 -0500

    Add Network_Info class

--- wifi_driver/wifi_driver/wifi_driver_node.py
+++ wifi_driver/wifi_driver/wifi_driver_node.py
@@ -1,11 +1,121 @@
 import rclpy
 from rclpy.node import Node
 
-# from vn_interface.msg import Vectornav
+from wifi_interface.msg import WifiList, WifiMeasurement
+from queue import Queue
 import serial
 import time
 import scipy as sp
 
+
+class Network_Info():
+    """
+    Class for storing info on a network's RSSI values as they are measured multiple times
+    """
+
+    def __init__(self):
+        measurements = 0
+        total = -1
+        rssis = Queue(maxsize=5)
+        curr_rssi = -1
+        variance = -1
+        active = False
+
+    def _calc_variance(self):
+        """
+        Calculates the variance of the given rssis queue
+        """
+
+        if self.measurements > 0:
+            if self.measurements > 1:
+                diffs = 0
+                for rssi in rssis:
+                    if rssi != -1:
+                        diffs += (rssi - (self.total / self.measurements)) ** 2
+                self.variance = diffs / self.measurements
+            else:
+                self.variance = 0
+        else:
+            self.variance = -1
+
+    def update_info(self, rssi_measurement=-1):
+        """
+        Updates the attributes of the `Network_Info` class given a new rssi measurement
+
+        Parameters
+        ----------
+        rssi_measurement : int, default=-1
+            RSSI measurement to be added to the given `Network_Info` class
+        """
+            
+        out_val = self.rssis.get()
+
+        if out_val > 0 and rssi_measurement > 0:
+            self.total += (rssi_measurement - out_val)
+            self.active = True
+        else if out_val > 0 and rssi_measurement < 0:
+            self.total -= out_val
+            self.measurements -= 1
+            self.active = False
+        else if out_val < 0 and rssi_measurement > 0:
+            self.total += rssi_measurement
+            self.measurements += 1
+            self.active = True
+        else:
+            self.active = False
+   
+        self.rssis.put(rssi_measurement)
+        self.curr_rssi = rssi_measurement
+
+        self._calc_variance()
+
+    def is_active(self):
+        """
+        Returns if the network is currently active (meaning the last RSSI measurement is valid)
+
+        Returns
+        -------
+        bool
+            True if the network is active
+        """
+
+        return self.active
+
+    def is_unresponsive(self):
+        """
+        Returns if the network is currently unresponsive (meaning the last 5 RSSI measurements are invalid)
+
+        Returns
+        -------
+        bool
+            True if the network is unresponsive
+        """
+
+        return (self.measurements == 0)
+
+    def get_info(self):
+        """
+        Gets a useful status of the current network info
+
+        Returns
+        -------
+        Tuple[bool, int, float]
+            Tuple containing whether the network is active or not, the current measured RSSI, and the measured variance
+        """
+
+        return (self.active, self.curr_rssi, self.variance
+            
+    
+class WiFi_Manager():
+
+    def __init__(self):
+        wifi_bssids = {}
+        networks = 0
+
+    def scan_for_networks(self):
+        pass
+
+    def purge_unresponsive_networks(self):
 
 
 
@@ -13,6 +123,8 @@ class WifiDriver(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
+
+        
 
         # self.get_logger().info("Starting up!")
 
